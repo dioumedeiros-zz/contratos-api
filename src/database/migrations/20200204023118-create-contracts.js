92e1784a87593fb2cc501c9ad5c72155b5a5ff77
DiouMedeiros <diou.182@gmail.com>
2020-02-04 00:15:17 -0300

    feat: Created migrations

---
+++ src/database/migrations/20200204023118-create-contracts.js
@@ -0,0 +1,66 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('contracts', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      loan_value: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      monthly_rent: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      birth_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      material_status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      file_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('contracts');
+  },
+};
